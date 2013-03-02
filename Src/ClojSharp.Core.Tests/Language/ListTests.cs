651d9881fb6d99fc836f173df7c924d46ac5a9a4
ajlopez <webmaster@ajlopez.com>
2013-03-02 18:54:41 -0300

    List with first test

---
+++ Src/ClojSharp.Core.Tests/Language/ListTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ClojSharp.Core.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void MakeList()
+        {
+            List list = new List(1, 2);
+
+            Assert.AreEqual(1, list.First);
+            Assert.AreEqual(2, list.Rest);
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Language/List.cs
@@ -0,0 +1,23 @@
+﻿namespace ClojSharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class List
+    {
+        private object first;
+        private object rest;
+
+        public List(object first, object rest)
+        {
+            this.first = first;
+            this.rest = rest;
+        }
+
+        public object First { get { return this.first; } }
+
+        public object Rest { get { return this.rest; } }
+    }
+}
