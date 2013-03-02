5bde04791dc2b9ddf5db951a1c465eff69584954
ajlopez <webmaster@ajlopez.com>
2013-03-02 17:44:07 -0300

    Lexer first test

---
+++ Src/ClojSharp.Core.Tests/Compiler/LexerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ClojSharp.Core.Tests.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Compiler;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        public void GetNull()
+        {
+            Lexer lexer = new Lexer(null);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Compiler/Lexer.cs
@@ -0,0 +1,19 @@
+﻿namespace ClojSharp.Core.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Lexer
+    {
+        public Lexer(string text)
+        {
+        }
+
+        public object NextToken()
+        {
+            return null;
+        }
+    }
+}
