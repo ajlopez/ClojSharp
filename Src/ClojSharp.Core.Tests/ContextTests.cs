e44b4aeab54e3d9de3fb3951f61a5a8f2b890292
ajlopez <webmaster@ajlopez.com>
2013-03-02 19:32:21 -0300

    Context with first test

---
+++ Src/ClojSharp.Core.Tests/ContextTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ClojSharp.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetNullIfUndefined()
+        {
+            Context context = new Context();
+
+            Assert.IsNull(context.GetValue("name"));
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Context.cs
@@ -0,0 +1,15 @@
+﻿namespace ClojSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Context
+    {
+        public object GetValue(string name)
+        {
+            return null;
+        }
+    }
+}
