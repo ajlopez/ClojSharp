6d81c744d5406b75e6e9ad2e2efea1f123e5f474
ajlopez <webmaster@ajlopez.com>
2013-03-02 18:33:40 -0300

    Parser first test

---
+++ Src/ClojSharp.Core.Tests/Compiler/ParserTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ClojSharp.Core.Tests.Compiler
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Compiler;
+
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void ParseNull()
+        {
+            Parser parser = new Parser(null);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Compiler/Parser.cs
@@ -0,0 +1,19 @@
+﻿namespace ClojSharp.Core.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Parser
+    {
+        public Parser(string text)
+        {
+        }
+
+        public object ParseExpression()
+        {
+            return null;
+        }
+    }
+}
