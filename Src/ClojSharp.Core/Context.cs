22e6b87b14457ea4c082115b11b018c704033bc1
ajlopez <webmaster@ajlopez.com>
2013-03-02 19:34:24 -0300

    Context get set value

--- Src/ClojSharp.Core/Context.cs
+++ Src/ClojSharp.Core/Context.cs
@@ -7,8 +7,18 @@
 
     public class Context
     {
+        private IDictionary<string, object> values = new Dictionary<string, object>();
+
+        public void SetValue(string name, object value)
+        {
+            values[name] = value;
+        }
+
         public object GetValue(string name)
         {
+            if (values.ContainsKey(name))
+                return values[name];
+
             return null;
         }
     }
