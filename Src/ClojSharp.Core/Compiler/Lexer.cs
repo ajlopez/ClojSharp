23c2fe80e5c19c453e6c16bc31bcd80ed0665560
ajlopez <webmaster@ajlopez.com>
2013-03-02 17:58:45 -0300

    Lexer.NextInteger with first test

--- Src/ClojSharp.Core/Compiler/Lexer.cs
+++ Src/ClojSharp.Core/Compiler/Lexer.cs
@@ -24,13 +24,37 @@
         {
             string value = string.Empty;
 
+            int ch = this.NextChar();
+
+            if (ch < 0)
+                return null;
+
+            char chr = (char)ch;
+
+            if (char.IsDigit(chr))
+                return this.NextInteger(chr);
+
+            return this.NextName(chr);
+        }
+
+        private Token NextName(char chr)
+        {
+            string value = chr.ToString();
+
             for (int ch = this.NextChar(); ch >= 0; ch = this.NextChar())
                 value += (char)ch;
 
-            if (string.IsNullOrEmpty(value))
-                return null;
-
             return new Token(TokenType.Name, value);
+        }
+
+        private Token NextInteger(char chr)
+        {
+            string value = chr.ToString();
+
+            for (int ch = this.NextChar(); ch >= 0 && char.IsDigit((char)ch); ch = this.NextChar())
+                value += (char)ch;
+
+            return new Token(TokenType.Integer, value);
         }
 
         private int NextChar()
--- Src/ClojSharp.Core/Compiler/TokenType.cs
+++ Src/ClojSharp.Core/Compiler/TokenType.cs
@@ -7,6 +7,7 @@
 
     public enum TokenType
     {
-        Name = 1
+        Name = 1,
+        Integer = 2
     }
 }
