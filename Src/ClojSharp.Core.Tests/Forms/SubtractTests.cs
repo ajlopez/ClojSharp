273d32b41e36ee31a8b42f631ece73338bba84c0
ajlopez <webmaster@ajlopez.com>
2013-03-03 09:01:51 -0300

    Subtract two integers

---
+++ Src/ClojSharp.Core.Tests/Forms/SubtractTests.cs
@@ -0,0 +1,20 @@
+﻿namespace ClojSharp.Core.Tests.Forms
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Forms;
+
+    [TestClass]
+    public class SubtractTests
+    {
+        [TestMethod]
+        public void SubtractTwoIntegers()
+        {
+            Subtract subtract = new Subtract();
+            Assert.AreEqual(-1, subtract.Evaluate(null, new object[] { 1, 2 }));
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Forms/Subtract.cs
@@ -0,0 +1,15 @@
+﻿namespace ClojSharp.Core.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Subtract : BaseForm
+    {
+        public override object EvaluateForm(Context context, IList<object> arguments)
+        {
+            return (int)arguments[0] - (int)arguments[1];
+        }
+    }
+}
