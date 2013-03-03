5669db3ffb0fd80eae76228932523e36881bd223
ajlopez <webmaster@ajlopez.com>
2013-03-03 18:40:40 -0300

    First with tests

---
+++ Src/ClojSharp.Core.Tests/Forms/FirstTests.cs
@@ -0,0 +1,63 @@
+﻿namespace ClojSharp.Core.Tests.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Forms;
+    using ClojSharp.Core.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClojSharp.Core.Exceptions;
+
+    [TestClass]
+    public class FirstTests
+    {
+        [TestMethod]
+        public void FirstInSimpleList()
+        {
+            First first = new First();
+            Machine machine = new Machine();
+
+            Assert.AreEqual(1, first.Evaluate(machine.RootContext, new object[] { new List(new Symbol("list"), new List(1, null)) }));
+        }
+
+        [TestMethod]
+        public void FirstToNil()
+        {
+            First first = new First();
+            Assert.IsNull(first.Evaluate(null, new object[] { null }));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RaiseIfArgumentIsNotAnISeq()
+        {
+            First first = new First();
+            first.Evaluate(null, new object[] { 1 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArityException))]
+        public void RaiseIfNullArguments()
+        {
+            First first = new First();
+            first.Evaluate(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArityException))]
+        public void RaiseIfNoArgument()
+        {
+            First first = new First();
+            first.Evaluate(null, new object[] { });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArityException))]
+        public void RaiseIfTwoArguments()
+        {
+            First first = new First();
+            first.Evaluate(null, new object[] { 1, 2 });
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Forms/First.cs
@@ -0,0 +1,26 @@
+﻿namespace ClojSharp.Core.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Language;
+
+    public class First : BaseForm
+    {
+        public override int RequiredArity { get { return 1; } }
+
+        public override bool VariableArity { get { return false; } }
+
+        public override object EvaluateForm(Context context, IList<object> arguments)
+        {
+            if (arguments[0] == null)
+                return null;
+
+            if (!(arguments[0] is ISeq))
+                throw new ArgumentException(string.Format("Don't know how to create ISeq from {0}", arguments[0].GetType().FullName));
+
+            return ((ISeq)arguments[0]).First;
+        }
+    }
+}
