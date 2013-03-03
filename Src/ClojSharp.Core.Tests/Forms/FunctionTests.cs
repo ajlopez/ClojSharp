61f4aba57f976847779775531233cd7f9aefee8b
ajlopez <webmaster@ajlopez.com>
2013-03-03 10:37:04 -0300

    Function with first test

---
+++ Src/ClojSharp.Core.Tests/Forms/FunctionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ClojSharp.Core.Tests.Forms
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Forms;
+
+    [TestClass]
+    public class FunctionTests
+    {
+        [TestMethod]
+        public void FunctionEvaluateString()
+        {
+            Function function = new Function(null, null, "foo");
+
+            Assert.AreEqual("foo", function.Evaluate(null, null));
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Forms/Function.cs
@@ -0,0 +1,26 @@
+﻿namespace ClojSharp.Core.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Function : BaseForm
+    {
+        private Context closure;
+        private IList<string> argumentnames;
+        private object body;
+
+        public Function(Context closure, IList<string> argumentnames, object body)
+        {
+            this.closure = closure;
+            this.argumentnames = argumentnames;
+            this.body = body;
+        }
+
+        public override object EvaluateForm(Context context, IList<object> arguments)
+        {
+            return this.body;
+        }
+    }
+}
