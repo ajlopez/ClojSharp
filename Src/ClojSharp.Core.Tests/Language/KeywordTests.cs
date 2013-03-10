ebc93dc614109936db23b662cc2a9732f18857c1
ajlopez <webmaster@ajlopez.com>
2013-03-10 19:33:11 -0300

    Keyword first tests

---
+++ Src/ClojSharp.Core.Tests/Language/KeywordTests.cs
@@ -0,0 +1,39 @@
+﻿namespace ClojSharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Language;
+
+    [TestClass]
+    public class KeywordTests
+    {
+        [TestMethod]
+        public void CreateKeyword()
+        {
+            Keyword keyword = new Keyword("a");
+
+            Assert.AreEqual("a", keyword.Name);
+            Assert.AreEqual(":a", keyword.ToString());
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            Keyword keyword1 = new Keyword("a");
+            Keyword keyword2 = new Keyword("b");
+            Keyword keyword3 = new Keyword("a");
+
+            Assert.IsTrue(keyword1.Equals(keyword3));
+            Assert.IsTrue(keyword3.Equals(keyword1));
+            Assert.AreEqual(keyword1.GetHashCode(), keyword3.GetHashCode());
+
+            Assert.IsFalse(keyword1.Equals(null));
+            Assert.IsFalse(keyword1.Equals(123));
+            Assert.IsFalse(keyword1.Equals(keyword2));
+            Assert.IsFalse(keyword2.Equals(keyword1));
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Language/Keyword.cs
@@ -0,0 +1,44 @@
+﻿namespace ClojSharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Keyword
+    {
+        private string name;
+
+        public Keyword(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public override string ToString()
+        {
+            return ":" + this.name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is Keyword)
+            {
+                var keyword = (Keyword)obj;
+
+                return this.Name.Equals(keyword.Name);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + 17;
+        }
+    }
+}
