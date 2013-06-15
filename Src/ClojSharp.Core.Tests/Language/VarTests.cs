71e5eeb54bd7d8ceec0bb06f260a4dc90c96c710
ajlopez <webmaster@ajlopez.com>
2013-06-15 19:50:41 -0300

    Var with name, full name, value, namespace, first test

---
+++ Src/ClojSharp.Core.Tests/Language/VarTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ClojSharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Language;
+
+    [TestClass]
+    public class VarTests
+    {
+        [TestMethod]
+        public void CreateWithName()
+        {
+            Var var = new Var("foo");
+
+            Assert.AreEqual("foo", var.Name);
+            Assert.AreEqual("user/foo", var.FullName);
+            Assert.IsNull(var.Value);
+        }
+    }
+}
--- Src/ClojSharp.Core/Language/Var.cs
+++ Src/ClojSharp.Core/Language/Var.cs
@@ -7,13 +7,26 @@
 
     public class Var
     {
+        private string @namespace;
         private string name;
+        private object value;
 
         public Var(string name)
+            : this("user", name, null)
         {
+        }
+
+        public Var(string @namespace, string name, object value)
+        {
+            this.@namespace = @namespace;
             this.name = name;
+            this.value = value;
         }
 
         public string Name { get { return this.name; } }
+
+        public string FullName { get { return string.Format("{0}/{1}", this.@namespace, this.name); } }
+
+        public object Value { get { return this.value; } }
     }
 }
