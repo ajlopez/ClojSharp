efbcd394a3121ce53accf9e7f87de67b83ad5137
ajlopez <webmaster@ajlopez.com>
2014-05-11 19:44:46 -0300

    MultiFunction first tests

---
+++ Src/ClojSharp.Core.Tests/Forms/MultiFunctionTests.cs
@@ -0,0 +1,63 @@
+﻿namespace ClojSharp.Core.Tests.Forms
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Forms;
+    using ClojSharp.Core.Exceptions;
+
+    [TestClass]
+    public class MultiFunctionTests
+    {
+        [TestMethod]
+        public void EvaluateMultiFunction()
+        {
+            Function function1 = new Function(null, null, 0);
+            Function function2 = new Function(null, new string[] { "x" }, 1);
+            Function function3 = new Function(null, new string[] { "x", "y" }, 2);
+
+            MultiFunction function = new MultiFunction(new Function[] { function1, function2, function3 });
+
+            Assert.AreEqual(0, function.Evaluate(null, null));
+            Assert.AreEqual(0, function.Evaluate(null, new object[] { }));
+            Assert.AreEqual(1, function.Evaluate(null, new object[] { 1 }));
+            Assert.AreEqual(2, function.Evaluate(null, new object[] { 1, 2 }));
+        }
+
+        [TestMethod]
+        public void EvaluateMultiFunctionWithVariableArguments()
+        {
+            Function function1 = new Function(null, null, 0);
+            Function function2 = new Function(null, new string[] { "x" }, 1);
+            Function function3 = new Function(null, new string[] { "x", "y" }, 2);
+            Function function4 = new Function(null, new string[] { "x", "y" }, "rest", 3);
+
+            MultiFunction function = new MultiFunction(new Function[] { function1, function2, function3, function4 });
+
+            Assert.AreEqual(3, function.Evaluate(null, new object[] { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void RaiseIfInvalidArity()
+        {
+            Function function1 = new Function(null, null, 0);
+            Function function3 = new Function(null, new string[] { "x", "y" }, 2);
+            Function function4 = new Function(null, new string[] { "x", "y" }, "rest", 3);
+
+            MultiFunction function = new MultiFunction(new Function[] { function1, function3, function4 });
+
+            try
+            {
+                function.Evaluate(null, new object[] { 1 });
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(ArityException));
+                Assert.AreEqual("Wrong number of args (1) passed to ClojSharp.Core.Forms.MultiFunction", ex.Message);
+            }
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Forms/MultiFunction.cs
@@ -0,0 +1,39 @@
+﻿namespace ClojSharp.Core.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Exceptions;
+    using ClojSharp.Core.Language;
+
+    public class MultiFunction : BaseForm
+    {
+        private IList<Function> functions;
+
+        public MultiFunction(IList<Function> functions)
+        {
+            this.functions = functions;
+        }
+
+        public override int RequiredArity
+        {
+            get { return this.functions.Min(f => f.RequiredArity); }
+        }
+
+        public override bool VariableArity { get { return this.functions.Count > 1 || this.functions[0].VariableArity; } }
+
+        public override object EvaluateForm(IContext context, IList<object> arguments)
+        {
+            int arity = arguments == null ? 0 : arguments.Count;
+
+            foreach (var fn in this.functions)
+                if (fn.RequiredArity == arity)
+                    return fn.Evaluate(context, arguments);
+                else if (fn.RequiredArity < arity && fn.VariableArity)
+                    return fn.Evaluate(context, arguments);
+
+            throw new ArityException(this.GetType(), arity);
+        }
+    }
+}
