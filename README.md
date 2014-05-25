410d2065a9a083d6a7e179ea42c79e1cb5d99c4a
ajlopez <webmaster@ajlopez.com>
2014-05-25 18:00:21 -0300

    Console program, first version

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ A Clojure interpreter written in C#, Work in Progress.
 - [What does “^:static” do in Clojure?](http://stackoverflow.com/questions/7552632/what-does-static-do-in-clojure)    
 - [Lazy Sequences in Clojure](http://theatticlight.net/posts/Lazy-Sequences-in-Clojure/)
 - [The Weird and Wonderful Characters of Clojure](http://yobriefca.se/blog/2014/05/19/the-weird-and-wonderful-characters-of-clojure/)
+- [Clojure differences between Ref, Var, Agent, Atom, with examples](http://stackoverflow.com/questions/9132346/clojure-differences-between-ref-var-agent-atom-with-examples)
 
 ## Notes
 
---
+++ Src/ClojSharp.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClojSharp.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Compiler;
+    using ClojSharp.Core;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Machine machine = new Machine();
+            Parser parser = new Parser(Console.In);
+
+            while (true)
+            {
+                try
+                {
+                    var expr = parser.ParseExpression();
+                    Console.WriteLine(Machine.Evaluate(expr, machine.RootContext));
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
