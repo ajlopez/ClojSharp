598364f3d7b603a49d0084a3b37cae4c22a45f17
ajlopez <webmaster@ajlopez.com>
2014-11-09 08:42:38 -0300

    Macro with first test

---
+++ Src/ClojSharp.Core.Tests/SpecialForms/MacroTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ClojSharp.Core.Tests.SpecialForms
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Compiler;
+    using ClojSharp.Core.Forms;
+
+    [TestClass]
+    public class MacroTests
+    {
+        [TestMethod]
+        public void MakeAndEvaluateList()
+        {
+            Machine machine = new Machine();
+            Parser parser = new Parser("(cons 'list (cons x (cons y nil)))");
+            object body = parser.ParseExpression();
+            Macro macro = new Macro(machine.RootContext, new string[] { "x", "y" }, body);
+            var result = macro.Evaluate(machine.RootContext, new object[] { 1, 2 });
+            Assert.IsNotNull(result);
+            Assert.AreEqual("(1 2)", result.ToString());
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/SpecialForms/Macro.cs
@@ -0,0 +1,42 @@
+﻿namespace ClojSharp.Core.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Exceptions;
+    using ClojSharp.Core.Language;
+
+    public class Macro : IForm
+    {
+        private IContext closure;
+        private IList<string> names;
+        private object body;
+
+        public Macro(IContext closure, IList<string> names, object body)
+        {
+            this.closure = closure;
+            this.names = names;
+            this.body = body;
+        }
+
+        public object Evaluate(IContext context, IList<object> arguments)
+        {
+            IContext newcontext = new Context(this.closure);
+
+            if (this.names != null && this.names.Count > 0)
+                for (int k = 0; k < this.names.Count; k++)
+                    newcontext.SetValue(this.names[k], arguments[k]);
+
+            return Machine.Evaluate(Machine.Evaluate(this.body, newcontext), context);
+        }
+
+        private List MakeList(int nelement, IList<object> elements)
+        {
+            if (nelement >= elements.Count)
+                return null;
+
+            return new List(elements[nelement], this.MakeList(nelement + 1, elements));
+        }
+    }
+}
