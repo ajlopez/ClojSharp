c49c455aff697a5800d228588c2751034c22c8ec
ajlopez <webmaster@ajlopez.com>
2014-11-10 07:35:43 -0300

    MFn first test

---
+++ Src/ClojSharp.Core.Tests/SpecialForms/MFnTests.cs
@@ -0,0 +1,45 @@
+﻿namespace ClojSharp.Core.Tests.SpecialForms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Compiler;
+    using ClojSharp.Core.Exceptions;
+    using ClojSharp.Core.Forms;
+    using ClojSharp.Core.Language;
+    using ClojSharp.Core.SpecialForms;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MFnTests
+    {
+        private Context context;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.context = new Context();
+            this.context.SetValue("mfn", new MFn());
+            this.context.SetValue("cons", new Cons());
+            this.context.SetValue("quote", new Quote());
+            this.context.SetValue("list", new ListForm());
+        }
+
+        [TestMethod]
+        public void MakeList()
+        {
+            var result = this.EvaluateList("((mfn [x] (cons 'list (cons x nil))) 1)");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("(1)", result.ToString());
+        }
+
+        private object EvaluateList(string text)
+        {
+            Parser parser = new Parser(text);
+            var list = (List)parser.ParseExpression();
+            return list.Evaluate(this.context);
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/SpecialForms/MFn.cs
@@ -0,0 +1,36 @@
+﻿namespace ClojSharp.Core.SpecialForms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Forms;
+    using ClojSharp.Core.Language;
+
+    public class MFn : IForm
+    {
+        public object Evaluate(IContext context, IList<object> arguments)
+        {
+            return this.EvaluateMacro(context, arguments);
+        }
+
+        private Macro EvaluateMacro(IContext context, IList<object> arguments)
+        {
+            var elements = ((VectorValue)arguments[0]).Expressions;
+            IList<string> names = new List<string>();
+            int nelement = 0;
+
+            foreach (var element in elements)
+            {
+                string name = ((Symbol)element).Name;
+
+                names.Add(((Symbol)element).Name);
+                nelement++;
+            }
+
+            object body = arguments[1];
+
+            return new Macro(context, names, body);
+        }
+    }
+}
