43486635831ebf13cb018cb778c02641e8d45042
ajlopez <webmaster@ajlopez.com>
2014-12-07 03:58:32 -0300

    Machine loads core.clj, first tests

---
+++ Src/ClojSharp.Core.Tests/CoreTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ClojSharp.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ClojSharp.Core.Forms;
+    using ClojSharp.Core.Language;
+    using ClojSharp.Core.SpecialForms;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [DeploymentItem("Src", "Src")]
+    public class CoreTests
+    {
+        private Machine machine;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.machine = new Machine();
+            this.machine.EvaluateFile("Src\\core.clj");
+        }
+
+        [TestMethod]
+        public void MachineHasMacros()
+        {
+            this.IsMacro("defmacro");
+            this.IsMacro("defn");
+        }
+
+        private void IsMacro(string name)
+        {
+            var result = this.machine.RootContext.GetValue(name);
+            Assert.IsNotNull(result, name);
+            Assert.IsInstanceOfType(result, typeof(IForm), name);
+            Assert.IsInstanceOfType(result, typeof(Macro), name);
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Src/core.clj
@@ -0,0 +1,7 @@
+﻿(def defmacro (mfn [name & fdecl]
+	(list 'def name (cons 'mfn fdecl))
+))
+
+(defmacro defn [name & fdecl]
+	(list 'def name (cons 'fn fdecl))
+)
