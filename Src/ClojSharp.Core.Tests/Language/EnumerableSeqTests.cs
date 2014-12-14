6b49a560eaf97b4dbc5501e0d5bf9f6cc1754498
ajlopez <webmaster@ajlopez.com>
2014-12-14 04:47:52 -0300

    Seq over IEnumerable

---
+++ Src/ClojSharp.Core.Tests/Language/EnumerableSeqTests.cs
@@ -0,0 +1,57 @@
+﻿namespace ClojSharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClojSharp.Core.Language;
+
+    [TestClass]
+    public class EnumerableSeqTests
+    {
+        [TestMethod]
+        public void StringUsingNext()
+        {
+            var seq = EnumerableSeq.MakeSeq("foo");
+
+            Assert.AreEqual('f', seq.First);
+            Assert.AreEqual('o', seq.Next.First);
+            Assert.AreEqual('o', seq.Next.Next.First);
+            Assert.IsNull(seq.Next.Next.Next);
+        }
+
+        [TestMethod]
+        public void StringUsingNextInReverse()
+        {
+            var seq = EnumerableSeq.MakeSeq("foo");
+
+            Assert.AreEqual('o', seq.Next.Next.First);
+            Assert.AreEqual('o', seq.Next.First);
+            Assert.AreEqual('f', seq.First);
+            Assert.IsNull(seq.Next.Next.Next);
+        }
+
+        [TestMethod]
+        public void StringUsingRest()
+        {
+            var seq = EnumerableSeq.MakeSeq("foo");
+
+            Assert.AreEqual('f', seq.First);
+            Assert.AreEqual('o', seq.Rest.First);
+            Assert.AreEqual('o', seq.Rest.Rest.First);
+            Assert.IsInstanceOfType(seq.Rest.Rest.Rest, typeof(EmptyList));
+        }
+
+        [TestMethod]
+        public void StringUsingRestInReverse()
+        {
+            var seq = EnumerableSeq.MakeSeq("foo");
+
+            Assert.AreEqual('o', seq.Rest.Rest.First);
+            Assert.AreEqual('o', seq.Rest.First);
+            Assert.AreEqual('f', seq.First);
+            Assert.IsInstanceOfType(seq.Rest.Rest.Rest, typeof(EmptyList));
+        }
+    }
+}
---
+++ Src/ClojSharp.Core/Language/EnumerableSeq.cs
@@ -0,0 +1,78 @@
+﻿namespace ClojSharp.Core.Language
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class EnumerableSeq : ISeq
+    {
+        private IEnumerator enumerator;
+        private bool expanded;
+        private object first;
+        private ISeq rest;
+
+        public static ISeq MakeSeq(IEnumerable value)
+        {
+            return MakeSeq(value.GetEnumerator());
+        }
+
+        private static ISeq MakeSeq(IEnumerator enumerator)
+        {
+            if (enumerator.MoveNext())
+                return new EnumerableSeq(enumerator);
+
+            return EmptyList.Instance;
+        }
+
+        private EnumerableSeq(IEnumerator enumerator)
+        {
+            this.enumerator = enumerator;
+        }
+
+        private void Expand()
+        {
+            this.first = this.enumerator.Current;
+            this.rest = MakeSeq(this.enumerator);
+
+            this.expanded = true;
+        }
+
+        public object First
+        {
+            get 
+            {
+                if (!this.expanded)
+                    this.Expand();
+
+                return this.first;
+            }
+        }
+
+        public ISeq Next
+        {
+            get
+            {
+                if (!this.expanded)
+                    this.Expand();
+
+                if (this.rest is EmptyList)
+                    return null;
+
+                return this.rest;
+            }
+        }
+
+        public ISeq Rest
+        {
+            get
+            {
+                if (!this.expanded)
+                    this.Expand();
+
+                return this.rest;
+            }
+        }
+    }
+}
