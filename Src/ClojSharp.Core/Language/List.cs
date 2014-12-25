d6d694708d51bb2f8e9f0c4743c7bb1c298714ca
ajlopez <webmaster@ajlopez.com>
2014-12-25 18:46:29 -0300

    Fixing source analysis

--- Src/ClojSharp.Core/Language/List.cs
+++ Src/ClojSharp.Core/Language/List.cs
@@ -1,11 +1,11 @@
 ﻿namespace ClojSharp.Core.Language
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using ClojSharp.Core.Forms;
-using System.Collections;
 
     public class List : IEvaluable, ISeq
     {
